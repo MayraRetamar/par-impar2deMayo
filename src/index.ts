664e52aa12a19ebf1089d2d74224bdc72bc36233
MayraRetamar <104107235+MayraRetamar@users.noreply.github.com>
2022-05-05 19:14:06 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+let numero: number;
+let contador: number;
+while (numero > 0) {
+  numero = Number(prompt("Ingrese un numero"));
+  contador + 1;
+}
+if (numero % 2 === 0) {
+  console.log("El numero ingresado es par");
+} else {
+  console.log("El numero ingresado es impar");
+}
