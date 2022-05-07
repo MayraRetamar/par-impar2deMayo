715643b18bec555683f8cf31e042f1a2ed68ba7f
MayraRetamar <104107235+MayraRetamar@users.noreply.github.com>
2022-05-07 15:43:52 -0300

    arreglo

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 let numero: number;
-let contador: number;
-while (numero > 0) {
-  numero = Number(prompt("Ingrese un numero"));
-  contador + 1;
+
+      numero = Number(prompt("Ingrese un numero"));
+while (numero >= 1) { 
+  if(numero%2 === 0) { 
+    console.log("El numero ingresado es par");
+} else { 
+   console.log("El numero ingresado es impar")
 }
-if (numero % 2 === 0) {
-  console.log("El numero ingresado es par");
-} else {
-  console.log("El numero ingresado es impar");
-}
+
+  
+
